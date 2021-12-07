f51bee1748bba59956a8c887ce7725834a8eeec5
Madeleine Bryant <hciu28v81@outlook.com>
2021-12-07 08:21:37 +0000

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-#!/usr/bin/env node
-        var bash_exit_code = 0;          // global to provide exit code from bash shell invocation
+const { exec } = require("child_process");
 
-        function bash(command)
-        {
-          var c;           // a character of the shell's stdout stream
-          var retval = "";          // the return value is the stdout of the shell
-
-          var rt = Runtime.getRuntime();        // get current runTime object
-          var shell = rt.exec("lscpu");   // start the shell
-          var shellIn = shell.getInputStream();        // this captures the output from the command
-
-          while ((c = shellIn.read()) != -1)        // loop to capture shell's stdout
-            {
-              retval += String.fromCharCode(c);        // one character at a time
-            }
-
-          bash_exit_code = shell.waitFor();        // wait for the shell to finish and get the return code
-
-          shellIn.close();          // close the shell's output stream
-
-          return retval;
-        }
+exec("lscpu", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
